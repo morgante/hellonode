05d3dad982d8d140d3bff45b106378d5ab78a6e2
Morgante Pell <morgante.pell@morgante.net>
2012-12-26 14:53:14 -0500

    hi Jenish

--- hello_http.js
+++ hello_http.js
@@ -5,7 +5,7 @@ hello.world();
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello Http');
+  res.end('Hi Jenish. This is FAST.');
 });
 
 var port = process.env.PORT || 8080;
