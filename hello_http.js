21450a35d0178ed1fa27c5345f39a61e03f3034a
Morgante Pell <morgante.pell@morgante.net>
2012-12-26 14:29:05 -0500

    begin learning

---
+++ hello_http.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var hello = require('./hello');
+
+hello.world();
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+
+server.listen(8090);
